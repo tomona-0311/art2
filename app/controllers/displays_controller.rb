cdb9f1a57ffd4357dac6c8a1176b8fba37edafab
tomona-0311 <karafuru0311@gmail.com>
2023-07-06 01:47:03 +0900

    showの表示ができない

--- app/controllers/displays_controller.rb
+++ app/controllers/displays_controller.rb
@@ -3,27 +3,36 @@ class DisplaysController < ApplicationController
 
   def index
     @displays = Display.all
+    @user = current_user
+    binding.pry
   end
 
   def new
     @display = Display.new
+    @user = current_user
   end
 
   def create
   #display.image.attach(display_params[:image])
-
-    @display = Display.create(display_params)
-
+    @display = Display.create!(display_params)
     if @display.save
       puts "保存に成功しました"
           flash[:notion] = "作品登録完了"
-      redirect_to :displays# ＠displayとすることでDBから取得したデータを指定すると、そのコントローラーのshowアクションに該当するページに遷移する。「redirect_to パラメータ」
+      redirect_to @display # ＠displayとすることでDBから取得したデータを指定すると、そのコントローラーのshowアクションに該当するページに遷移する。「redirect_to パラメータ」
    else
     puts "保存に失敗しました"
      render "new"
    end
   end
 end
+
+def show
+
+
+  @display = Display.find(params[:id])
+  @user = current_user
+end
+
 private
 def display_params
 params.require(:display).permit(:image,:introduction,:display_name)
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'displays/index'
 
+
   get 'posts/index'
 
   get 'posts/new'
   resources :displays
   resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
